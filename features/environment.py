3022c2d1e13ad64262d2021461a71bd572b204e3
kervyn rivero <kervynriveromujica@gmail.com>
2024-06-05 21:45:06 -0700

    Integrate your tests with a Cloud Testing Platform

--- features/environment.py
+++ features/environment.py
@@ -3,10 +3,12 @@ from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 from webdriver_manager.firefox import GeckoDriverManager
 from app.application import Application
+from selenium.webdriver.chrome.options import Options
 
 
-def browser_init(context):
+def browser_init(context, scenario_name):
     """
+    :param scenario_name:
     :param context: Behave context
     """
     ###
@@ -26,15 +28,37 @@ def browser_init(context):
     ### FIREFOX BROWSER ###
     ###
 
-    driver_path = GeckoDriverManager().install()
-    service = Service(driver_path)
-    context.driver = webdriver.Firefox(service=service)
+    #driver_path = GeckoDriverManager().install()
+    #service = Service(driver_path)
+    #context.driver = webdriver.Firefox(service=service)
 
 
 
     #driver_path = ChromeDriverManager().install()
     #service = Service(driver_path)
     #context.driver = webdriver.Chrome(service=service)
+
+    ### BROWSERSTACK ###
+    #Register for BrowserStack, then grab it from https://www.browserstack.com/accounts/settings
+    bs_user = 'kervynrivero_hKq7jr'
+    bs_key = 'Kx8QpEfdqfsCsNeeWRNQ'
+    url = f'http://{bs_user}:{bs_key}@hub-cloud.browserstack.com/wd/hub'
+
+    options = Options()
+
+
+    bstack_options = {
+        "os": "Windows",
+        "osVersion": "11",
+         "browserName": "Edge",
+        "browserVersion": "latest",
+        'sessionName': scenario_name
+    }
+    options.set_capability('bstack:options', bstack_options)
+    context.driver = webdriver.Remote(command_executor=url, options=options)
+
+
+
 
     context.driver.maximize_window()
 
@@ -46,7 +70,7 @@ def browser_init(context):
 
 def before_scenario(context, scenario):
     print('\nStarted scenario: ', scenario.name)
-    browser_init(context)
+    browser_init(context, scenario.name)
 
 
 def before_step(context, step):
